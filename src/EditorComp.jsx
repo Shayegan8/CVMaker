326eab7eb0df4aabbe720229a6d42ce30363864c
Shayegan8 <shayeganrood85@gmail.com>
2026-02-06 16:30:55 +0330

    installed more dependencies for tiptap, html2pdf, added few buttons for writing article

--- src/EditorComp.jsx
+++ src/EditorComp.jsx
@@ -1,15 +1,23 @@
 import { EditorContent, useEditor } from "@tiptap/react"
 import StarterKit from "@tiptap/starter-kit"
 import Icons from "./Icons"
+import { TextStyle } from "@tiptap/extension-text-style"
+import { FontFamily } from "@tiptap/extension-text-style"
+import FontSize from "tiptap-extension-font-size"
+import TextAlign from "@tiptap/extension-text-align"
 
 export default function EditorComp() {
     const editor = useEditor({
-        extensions: [StarterKit],
+        extensions: [StarterKit, TextStyle, FontFamily, FontSize, TextAlign.configure({
+            types: ['heading', 'paragraph'],
+            alignments: ['left', 'center', 'right'],
+            defaultAlignment: ['left']
+        })],
         content: ""
     })
 
     return (<>
-        <EditorContent id="textshit" className="textbar" editor={editor} />
+        <EditorContent id="textshit" editor={editor} />
         <Icons editor={editor} /></>
     )
 
--- src/Icons.jsx
+++ src/Icons.jsx
@@ -1,5 +1,24 @@
+import dompurify from 'dompurify'
+import Left from './assets/left.svg?react'
+import Middle from './assets/middle.svg?react'
+import Right from './assets/right.svg?react'
+
 export default function Icons({ editor }) {
     if (!editor) return null
+
+    const sanitizedText = dompurify.sanitize(editor.getHTML(), {
+        ALLOWED_TAGS: ['b', 'i', 'u', 's', 'p', 'br', 'div', 'span'],
+        ALLOWED_ATTR: ['style'],
+        FORCE_BODY: [true]
+    })
+
+    const options = {
+        margin: 10,
+        filename: 'cv.pdf',
+        image: { type: 'jpeg', quality: 0.98 },
+        html2canvas: { scale: 2 }, // Higher scale = better quality
+        jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }
+    }
 
     return (<>
         <div className="icons">
@@ -9,6 +28,15 @@ export default function Icons({ editor }) {
                 </li>
                 <li id="italic" onClick={() => editor.chain().focus().toggleItalic().run()}>
                     I
+                </li>
+                <li>
+                    <Left id="left" onClick={() => editor.chain().focus().setTextAlign('left').run()} />
+                </li>
+                <li>
+                    <Middle id="middle" onClick={() => editor.chain().focus().setTextAlign('center').run()} />
+                </li>
+                <li>
+                    <Right id="right" onClick={() => editor.chain().focus().setTextAlign('right').run()} />
                 </li>
             </ul>
         </div>
