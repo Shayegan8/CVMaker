020ee280aa487eaaaf115e277b7aa97b09e3ae4e
Shayegan8 <shayeganrood85@gmail.com>
2026-02-09 14:17:50 +0330

    fixed

--- src/TopHeader.jsx
+++ src/TopHeader.jsx
@@ -1,6 +1,3 @@
-import { useState } from "react"
-
 export default function TopHeader({ value }) {
-    const small = window.innerWidth < 616
-    return <div className={small ? "topheader-small" : "topheader"}>{value}</div>
+    return <div className="topheader">{value}</div>
 }
