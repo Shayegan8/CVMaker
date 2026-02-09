0cf548c4a2ee99030135d78b93e9f0cdd13c76b2
Shayegan8 <shayeganrood85@gmail.com>
2026-02-09 14:11:12 +0330

    TopHeader fixed, npm run build

--- src/TopHeader.jsx
+++ src/TopHeader.jsx
@@ -1,16 +1,6 @@
 import { useState } from "react"
 
 export default function TopHeader({ value }) {
-    const [content, setContent] = useState(null)
-    if (window.innerWidth < 616) {
-        setContent(<div className="topheader-small">
-            {value}
-        </div >)
-    } else
-        setContent(
-            <div className="topheader">
-                {value}
-            </div >
-        )
-    return content
+    const small = window.innerWidth < 616
+    return <div className={small ? "topheader-small" : "topheader"}>{value}</div>
 }
